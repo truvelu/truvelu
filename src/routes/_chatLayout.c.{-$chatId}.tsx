4e283b9fd95d58896aa42cb9688cd372821fbc6b
Ilyas Adiyasa <ilyas.adiyasa@erajaya.com>
2025-10-21 23:10:13 +0700

    feat: fix isLoading state in some page

--- src/routes/_chatLayout.c.{-$chatId}.tsx
+++ src/routes/_chatLayout.c.{-$chatId}.tsx
@@ -1,34 +1,39 @@
-import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
 import AiConversation from "@/components/shared/ai-conversation";
-import { useConvexAuth } from "convex/react";
 import { Button } from "@/components/ui/button";
+import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
+import { useConvexAuth } from "convex/react";
 
 export const Route = createFileRoute("/_chatLayout/c/{-$chatId}")({
-  component: App,
-  beforeLoad: (context) => {
-    if (!context.params.chatId) {
-      throw redirect({
-        to: "/",
-      });
-    }
-  },
+	component: App,
+	beforeLoad: (context) => {
+		if (!context.params.chatId) {
+			throw redirect({
+				to: "/",
+			});
+		}
+	},
 });
 
 function App() {
-  const { isAuthenticated, isLoading } = useConvexAuth();
-  const navigate = useNavigate();
+	const { isAuthenticated, isLoading } = useConvexAuth();
+	const navigate = useNavigate();
 
-  if (!isLoading) {
-    return <div className="p-4">Loading...</div>;
-  }
+	if (isLoading) {
+		return <div className="p-4">Loading...</div>;
+	}
 
-  if (!isAuthenticated) {
-    return (
-      <div className="p-4">
-        <Button onClick={() => navigate({ to: "/auth" })} className="w-full rounded-tmedium cursor-pointer">Sign in</Button>
-      </div>
-    );
-  }
+	if (!isAuthenticated) {
+		return (
+			<div className="p-4">
+				<Button
+					onClick={() => navigate({ to: "/auth" })}
+					className="w-full rounded-tmedium cursor-pointer"
+				>
+					Sign in
+				</Button>
+			</div>
+		);
+	}
 
-  return <AiConversation />;
+	return <AiConversation />;
 }
--- src/routes/_chatLayout.l.{-$learningId}_.c.{-$chatId}.tsx
+++ src/routes/_chatLayout.l.{-$learningId}_.c.{-$chatId}.tsx
@@ -4,39 +4,44 @@ import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
 import { useConvexAuth } from "convex/react";
 
 export const Route = createFileRoute(
-  "/_chatLayout/l/{-$learningId}_/c/{-$chatId}"
+	"/_chatLayout/l/{-$learningId}_/c/{-$chatId}",
 )({
-  component: RouteComponent,
-  beforeLoad: (context) => {
-    if (!!context.params.learningId && !context.params.chatId) {
-      throw redirect({
-        to: "/l/{-$learningId}",
-      });
-    }
+	component: RouteComponent,
+	beforeLoad: (context) => {
+		if (!!context.params.learningId && !context.params.chatId) {
+			throw redirect({
+				to: "/l/{-$learningId}",
+			});
+		}
 
-    if (!context.params.learningId && !context.params.chatId) {
-      throw redirect({
-        to: "/",
-      });
-    }
-  },
+		if (!context.params.learningId && !context.params.chatId) {
+			throw redirect({
+				to: "/",
+			});
+		}
+	},
 });
 
 function RouteComponent() {
-  const { isAuthenticated, isLoading } = useConvexAuth();
-  const navigate = useNavigate()
+	const { isAuthenticated, isLoading } = useConvexAuth();
+	const navigate = useNavigate();
 
-  if (!isLoading) {
-    return <div className="p-4">Loading...</div>;
-  }
+	if (isLoading) {
+		return <div className="p-4">Loading...</div>;
+	}
 
-  if (!isAuthenticated) {
-    return (
-      <div className="p-4">
-        <Button onClick={() => navigate({ to: "/auth" })} className="w-full rounded-tmedium cursor-pointer">Sign in</Button>
-      </div>
-    );
-  }
+	if (!isAuthenticated) {
+		return (
+			<div className="p-4">
+				<Button
+					onClick={() => navigate({ to: "/auth" })}
+					className="w-full rounded-tmedium cursor-pointer"
+				>
+					Sign in
+				</Button>
+			</div>
+		);
+	}
 
-  return <AiConversation />;
+	return <AiConversation />;
 }
